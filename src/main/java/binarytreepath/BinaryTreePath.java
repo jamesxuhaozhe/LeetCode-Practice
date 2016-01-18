be9b471993f5e26aac7627d399000bb9637d7f66
Haozhe Xu <jamesxuhaozhe@gmail.com>
2016-01-18 17:09:11 +0800

    add binary tree path

---
+++ src/main/java/binarytreepath/BinaryTreePath.java
@@ -0,0 +1,42 @@
+package binarytreepath;
+
+import datastructure.TreeNode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by haozhexu on 1/18/16.
+ */
+public class BinaryTreePath {
+    public List<String> binaryTreePath(TreeNode root) {
+        List<String> result = new ArrayList<>();
+        if (root == null) {
+            return result;
+        }
+        if (root.left == null && root.right == null) {
+            result.add(Integer.toString(root.val));
+            return result;
+        }
+        if (root.left != null) {
+            createPath(root.left, Integer.toString(root.val), result);
+        }
+        if (root.right != null) {
+            createPath(root.right, Integer.toString(root.val), result);
+        }
+        return result;
+    }
+
+    private void createPath(TreeNode node, String current, List<String> result) {
+        current += "->" + Integer.toString(node.val);
+        if (node.left == null && node.right == null) {
+            result.add(current);
+        }
+        if (node.left != null) {
+            createPath(node.left, current, result);
+        }
+        if (node.right != null) {
+            createPath(node.right, current, result);
+        }
+    }
+}
---
+++ src/test/java/tree/binarytreepath/BinaryTreePathTest.java
@@ -0,0 +1,35 @@
+package tree.binarytreepath;
+
+import binarytreepath.BinaryTreePath;
+import datastructure.TreeNode;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+/**
+ * Created by haozhexu on 1/18/16.
+ */
+public class BinaryTreePathTest {
+    private BinaryTreePath algo;
+    private TreeNode tree;
+
+    @Before
+    public void setUp() throws Exception {
+        algo = new BinaryTreePath();
+        tree = new TreeNode(1);
+        TreeNode node2 = new TreeNode(2);
+        TreeNode node7 = new TreeNode(7);
+        TreeNode node1 = new TreeNode(1);
+        TreeNode node3 = new TreeNode(3);
+        tree.left = node2;
+        tree.right = node7;
+        node2.left = node1;
+        node2.right = node3;
+    }                                                                                 git
+
+    @Test
+    public void testBinaryTreePath() throws Exception {
+        String expected = "[1->2->1, 1->2->3, 1->7]";
+        assertEquals(expected, algo.binaryTreePath(tree).toString());
+    }
+}
