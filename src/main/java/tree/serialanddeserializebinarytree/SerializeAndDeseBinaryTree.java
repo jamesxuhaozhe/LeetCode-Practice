0b63338bdef21cae0955a887530cbea223b90bf6
Haozhe Xu <jamesxuhaozhe@gmail.com>
2016-01-19 13:47:45 +0800

    start serial and deserial binary tree

---
+++ src/main/java/tree/serialanddeserializebinarytree/SerializeAndDeseBinaryTree.java
@@ -0,0 +1,26 @@
+package tree.serialanddeserializebinarytree;
+
+import datastructure.TreeNode;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+/**
+ * Created by haozhexu on 1/19/16.
+ */
+public class SerializeAndDeseBinaryTree {
+    // Encodes a tree to a single string.
+    private final static String NULL_TREE = "null_tree";
+
+    public String serialize(TreeNode root) {
+
+
+    }
+
+    // Decodes your encoded data to tree.
+    public TreeNode deserialize(String data) {
+
+    }
+}
